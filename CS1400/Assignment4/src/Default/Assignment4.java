9beec2aae04ee3d42e31eaf101be01434c22affb
JohnStewart1324 <Johnstewart2500@gmail.com>
2023-10-23 18:03:32 -0700

    Assignment4

---
+++ CS1400/Assignment4/src/Default/Assignment4.java
@@ -0,0 +1,27 @@
+package Default;
+/*
+//AUTHOR: YOUR NAME
+//FILENAME: TITLE OF THIS SOURCE FILE
+//SPECIFICATION: DESCRIPTION OF THIS PROGRAM
+//FOR: CS 1400 - ASSIGNMENT #4
+//TIME SPENT: HOW LONG IT TOOK YOU TO FINISH THIS ASSIGNMENT
+//*/
+
+import java.util.Scanner;
+
+public class Assignment4 {
+	public static void Main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int userInput;
+		
+		userInput = scan.nextInt();
+		
+		isPrime();
+	}
+	
+	public String isPrime() {
+		if(userInput % 2 == 0) {
+			
+		}
+	}
+}
